1c2e2cdea9ff7ef83f16516cd8f05cde92218b64
Jovan <jovansr@protonmail.com>
2024-07-08 21:16:44 +0200

    DEV-1: Refactor the address service for better readability

--- __tests__/services/addressService.test.ts
+++ __tests__/services/addressService.test.ts
@@ -2,21 +2,34 @@ import { getAddressDetails } from "../../backend/services/addressService";
 
 jest.mock("../../backend/services/addressService");
 
-test("should fetch transaction count for an address", async () => {
-  const expectedTransactionCount = 25;
+test("should fetch total volume transactions along with number of contracts, last active, and how old is the account", async () => {
+  const totalVolume = 1000;
+  const numberOfInteractedContracts = 25;
+  const gasSpent = 0.2;
+  const lastActive = 12512325123; // timestamp
+  const activity = 1823984219; // timestamp
+
   const address = "0xdFd1656602DB564661522daAd0ADB0b98d581050";
 
   // Mock implementation
   (getAddressDetails as jest.Mock).mockResolvedValueOnce({
     address,
-    transactionCount: expectedTransactionCount,
+    totalVolume,
+    numberOfInteractedContracts,
+    gasSpent,
+    lastActive,
+    activity,
   });
 
   const details = await getAddressDetails(address);
 
   expect(details).toEqual({
     address,
-    transactionCount: expectedTransactionCount,
+    totalVolume,
+    numberOfInteractedContracts,
+    gasSpent,
+    lastActive,
+    activity,
   });
 
   expect(getAddressDetails).toHaveBeenCalledTimes(1);
--- backend/services/addressService.ts
+++ backend/services/addressService.ts
@@ -1,4 +1,12 @@
-import { Network, Alchemy, AssetTransfersCategory } from "alchemy-sdk";
+import {
+  Network,
+  Alchemy,
+  AssetTransfersCategory,
+  RawContract,
+  ERC1155Metadata,
+  AssetTransfersResponse,
+  AssetTransfersResult,
+} from "alchemy-sdk";
 import dotenv from "dotenv";
 
 dotenv.config(); // Load environment variables
@@ -8,31 +16,110 @@ const settings = {
   apiKey: apiKey,
   network: Network.BASE_MAINNET,
 };
+const alchemy = new Alchemy(settings);
 
-export const getAddressDetails = async (address: string): Promise<void> => {
+export const getAddressDetails = async (
+  address: string
+): Promise<{ volume: number; uniqueContracts: number | undefined }> => {
   try {
-    const alchemy = new Alchemy(settings);
+    const response = await getAssetTransfers(address);
 
-    const response = await alchemy.core.getAssetTransfers({
+    const volume = await getVolume(response.transfers, address);
+    const uniqueContracts = (
+      await getUniqueContractAddresses(response.transfers)
+    )?.length;
+
+    return { volume, uniqueContracts };
+  } catch (error: any) {
+    console.error("Error fetching address details: " + error.message);
+    throw error;
+  }
+};
+
+async function isContractAddress(addr: string): Promise<boolean> {
+  return await alchemy.core.isContractAddress(addr);
+}
+
+async function getAssetTransfers(
+  address: string
+): Promise<AssetTransfersResponse> {
+  try {
+    return await alchemy.core.getAssetTransfers({
       fromAddress: address,
       excludeZeroValue: true,
       category: [
         AssetTransfersCategory.EXTERNAL,
         AssetTransfersCategory.ERC20,
         AssetTransfersCategory.ERC721,
+        AssetTransfersCategory.ERC1155,
       ],
     });
-
-    const volume = response.transfers.reduce((acc, item) => {
-      if (item.from.toLowerCase() == address.toLowerCase()) {
-        if (item.value) {
-          return acc + item.value;
-        }
-      }
-      return 0;
-    }, 0);
   } catch (error: any) {
-    console.error("Error fetching address details: " + error.message);
+    console.error("Something went wrong " + error.message);
     throw error;
   }
-};
+}
+
+/**
+ * Calculates the total volume of tokens transferred from the specified address.
+ *
+ * @param transfers - An array of AssetTransfersResult objects representing the token transfers.
+ * @param address - The address from which the volume should be calculated.
+ * @returns A Promise that resolves to the total volume of tokens transferred from the specified address.
+ */
+async function getVolume(
+  transfers: AssetTransfersResult[],
+  address: string
+): Promise<number> {
+  return transfers.reduce((acc, item) => {
+    if (item.from.toLowerCase() == address.toLowerCase()) {
+      if (item.value) {
+        return acc + item.value;
+      }
+    }
+    return 0;
+  }, 0);
+}
+
+/**
+ * Fetches the contract address associated with the given address.
+ *
+ * @param addr - The address to fetch the contract address for.
+ * @returns A Promise that resolves to the contract address if found, or null if not found.
+ */
+async function getContractAddresses(
+  transfers: AssetTransfersResult[]
+): Promise<Promise<string[] | null>> {
+  return Promise.all(
+    transfers.map(async (item) => {
+      if (item.to && (await alchemy.core.isContractAddress(item.to))) {
+        return item.to;
+      }
+
+      return null;
+    })
+  ).then((result) => {
+    return result.filter((item): item is string => item !== null) as string[];
+  });
+}
+
+/**
+ * Fetches the unique contract addresses associated with the given address.
+ *
+ * @param transfers - An array of AssetTransfersResult objects representing the token transfers.
+ * @returns A Promise that resolves to an array of unique contract addresses associated with the given address, or null if no contract addresses are found.
+ */
+async function getUniqueContractAddresses(
+  transfers: AssetTransfersResult[]
+): Promise<string[] | null> {
+  const contractAddresses = await getContractAddresses(transfers);
+
+  if (!contractAddresses) return null;
+
+  return contractAddresses.reduce<string[]>((acc, addr) => {
+    if (!acc.some((item) => item == addr)) {
+      acc.push(addr);
+    }
+    return acc;
+  }, []);
+}
