a040459e7325162027d8d096da581afcdbc537db
Jovan <jovansr@protonmail.com>
2024-07-20 18:53:05 +0200

    DEV-1: Add test timeout

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.spec.ts'],
-  testPathIgnorePatterns: ['/node_modules/']
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.spec.ts"],
+  testPathIgnorePatterns: ["/node_modules/"],
+  testTimeout: 30000,
 };
